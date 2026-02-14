b877feddf765d375979ae033b50116c186b2ee5d
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-14 13:28:04 +0530

    Update companyRemark.engine.js

--- js/engine/summaries/companyRemark.engine.js
+++ js/engine/summaries/companyRemark.engine.js
@@ -1,9 +1,17 @@
 import { computedStore } from "../../store/computed.store.js";
+import { dataStore } from "../../store/data.store.js";
 
 export function buildCompanyRemark() {
 
   const master = computedStore.master;
   if (!master) return;
+
+  const saleDays = dataStore.saleDays;
+
+  let totalDays = 0;
+  saleDays.forEach(row => {
+    totalDays += Number(row["Days"] || 0);
+  });
 
   const remarkMap = {};
 
@@ -12,11 +20,47 @@ export function buildCompanyRemark() {
     const remark = style.remark || "-";
 
     if (!remarkMap[remark]) {
-      remarkMap[remark] = 0;
+      remarkMap[remark] = {
+        totalUnits: 0,
+        totalStock: 0
+      };
     }
 
-    remarkMap[remark] += style.totalSales;
+    remarkMap[remark].totalUnits += style.totalSales;
+    remarkMap[remark].totalStock += style.totalStock;
   });
 
-  computedStore.summaries.companyRemark = remarkMap;
+  let grandUnits = 0;
+  let grandStock = 0;
+
+  const rows = Object.keys(remarkMap).map(remark => {
+
+    const totalUnits = remarkMap[remark].totalUnits;
+    const totalStock = remarkMap[remark].totalStock;
+
+    const drr = totalDays > 0 ? totalUnits / totalDays : 0;
+    const sc = drr > 0 ? totalStock / drr : 0;
+
+    grandUnits += totalUnits;
+    grandStock += totalStock;
+
+    return {
+      remark,
+      totalUnits,
+      totalStock,
+      drr,
+      sc
+    };
+  });
+
+  const grandDRR = totalDays > 0 ? grandUnits / totalDays : 0;
+  const grandSC = grandDRR > 0 ? grandStock / grandDRR : 0;
+
+  computedStore.summaries.companyRemark = {
+    rows,
+    grandUnits,
+    grandStock,
+    grandDRR,
+    grandSC
+  };
 }
