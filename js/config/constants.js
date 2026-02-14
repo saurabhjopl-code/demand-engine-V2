7989aa30a892eb1cb58030d03f72c4ef800315dd
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-14 16:24:13 +0530

    Update constants.js

---
+++ js/config/constants.js
@@ -0,0 +1,19 @@
+// js/config/constants.js
+
+export const SIZE_SEQUENCE = [
+  "FS",
+  "XS",
+  "S",
+  "M",
+  "L",
+  "XL",
+  "XXL",
+  "3XL",
+  "4XL",
+  "5XL",
+  "6XL",
+  "7XL",
+  "8XL",
+  "9XL",
+  "10XL"
+];
