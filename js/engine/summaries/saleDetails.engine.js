41e79fa833e625e4bc6704300f04ae8e634f66ac
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-14 13:10:09 +0530

    Update saleDetails.engine.js

--- js/engine/summaries/saleDetails.engine.js
+++ js/engine/summaries/saleDetails.engine.js
@@ -1,24 +1,66 @@
+import { dataStore } from "../../store/data.store.js";
 import { computedStore } from "../../store/computed.store.js";
 
 export function buildSaleDetails() {
 
-  const master = computedStore.master;
-  if (!master) return;
+  const sales = dataStore.sales;
+  const saleDays = dataStore.saleDays;
 
-  let totalSales = 0;
-  let totalStyles = 0;
-  let totalSkus = 0;
-
-  Object.values(master.styles).forEach(style => {
-    totalSales += style.totalSales;
-    totalStyles++;
-
-    totalSkus += Object.keys(style.skus).length;
+  const monthDaysMap = {};
+  saleDays.forEach(row => {
+    monthDaysMap[row["Month"]] = Number(row["Days"] || 0);
   });
 
+  const monthMap = {};
+
+  sales.forEach(row => {
+
+    const month = row["Month"];
+    const units = Number(row["Units"] || 0);
+
+    if (!month) return;
+
+    if (!monthMap[month]) {
+      monthMap[month] = {
+        totalUnits: 0,
+        days: monthDaysMap[month] || 0
+      };
+    }
+
+    monthMap[month].totalUnits += units;
+  });
+
+  let grandTotalUnits = 0;
+  let grandTotalDays = 0;
+
+  const result = [];
+
+  Object.keys(monthMap)
+    .sort()
+    .forEach(month => {
+
+      const totalUnits = monthMap[month].totalUnits;
+      const days = monthMap[month].days;
+
+      const drr = days > 0 ? totalUnits / days : 0;
+
+      grandTotalUnits += totalUnits;
+      grandTotalDays += days;
+
+      result.push({
+        month,
+        totalUnits,
+        drr
+      });
+    });
+
+  const grandDRR = grandTotalDays > 0
+    ? grandTotalUnits / grandTotalDays
+    : 0;
+
   computedStore.summaries.saleDetails = {
-    totalSales,
-    totalStyles,
-    totalSkus
+    rows: result,
+    grandTotalUnits,
+    grandDRR
   };
 }
