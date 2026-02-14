9a5d992d65f83ab1efc0838339d469872e322ec4
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-14 13:31:08 +0530

    Update category.engine.js

--- js/engine/summaries/category.engine.js
+++ js/engine/summaries/category.engine.js
@@ -1,9 +1,17 @@
 import { computedStore } from "../../store/computed.store.js";
+import { dataStore } from "../../store/data.store.js";
 
 export function buildCategory() {
 
   const master = computedStore.master;
   if (!master) return;
+
+  const saleDays = dataStore.saleDays;
+
+  let totalDays = 0;
+  saleDays.forEach(row => {
+    totalDays += Number(row["Days"] || 0);
+  });
 
   const categoryMap = {};
 
@@ -12,11 +20,47 @@ export function buildCategory() {
     const category = style.category || "-";
 
     if (!categoryMap[category]) {
-      categoryMap[category] = 0;
+      categoryMap[category] = {
+        totalUnits: 0,
+        totalStock: 0
+      };
     }
 
-    categoryMap[category] += style.totalSales;
+    categoryMap[category].totalUnits += style.totalSales;
+    categoryMap[category].totalStock += style.totalStock;
   });
 
-  computedStore.summaries.category = categoryMap;
+  let grandUnits = 0;
+  let grandStock = 0;
+
+  const rows = Object.keys(categoryMap).map(category => {
+
+    const totalUnits = categoryMap[category].totalUnits;
+    const totalStock = categoryMap[category].totalStock;
+
+    const drr = totalDays > 0 ? totalUnits / totalDays : 0;
+    const sc = drr > 0 ? totalStock / drr : 0;
+
+    grandUnits += totalUnits;
+    grandStock += totalStock;
+
+    return {
+      category,
+      totalUnits,
+      totalStock,
+      drr,
+      sc
+    };
+  });
+
+  const grandDRR = totalDays > 0 ? grandUnits / totalDays : 0;
+  const grandSC = grandDRR > 0 ? grandStock / grandDRR : 0;
+
+  computedStore.summaries.category = {
+    rows,
+    grandUnits,
+    grandStock,
+    grandDRR,
+    grandSC
+  };
 }
