4235664d652e4f2135623f1dff982ba24425c900
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-14 13:03:03 +0530

    Update data.store.js

---
+++ js/store/data.store.js
@@ -0,0 +1,11 @@
+export const dataStore = {
+  sales: [],
+  stock: [],
+  styleStatus: [],
+  saleDays: [],
+  sizeCount: [],
+  production: [],
+  meterCalc: [],
+  location: [],
+  xMarkup: []
+};
