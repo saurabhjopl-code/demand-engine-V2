c9a044071558594ebf547b8931ca104252a5f8aa
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-14 15:12:39 +0530

    Update report.index.js

--- js/engine/report.index.js
+++ js/engine/report.index.js
@@ -1,5 +1,7 @@
 import { buildDemand } from "./reports/demand.engine.js";
+import { buildOverstock } from "./reports/overstock.engine.js";
 
 export function buildAllReports() {
   buildDemand(45);
+  buildOverstock(90);
 }
