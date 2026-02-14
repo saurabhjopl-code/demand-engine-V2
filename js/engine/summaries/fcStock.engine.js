66e3244140193e51ad2aded682afe8e87cd16a63
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-14 13:14:48 +0530

    Update fcStock.engine.js

--- js/engine/summaries/fcStock.engine.js
+++ js/engine/summaries/fcStock.engine.js
@@ -1,3 +1,4 @@
+import { dataStore } from "../../store/data.store.js";
 import { computedStore } from "../../store/computed.store.js";
 
 export function buildFcStock() {
@@ -5,16 +6,70 @@ export function buildFcStock() {
   const master = computedStore.master;
   if (!master) return;
 
-  let totalStock = 0;
-  let totalProduction = 0;
+  const saleDays = dataStore.saleDays;
 
-  Object.values(master.styles).forEach(style => {
-    totalStock += style.totalStock;
-    totalProduction += style.totalProduction;
+  let totalDays = 0;
+  saleDays.forEach(row => {
+    totalDays += Number(row["Days"] || 0);
   });
 
+  const fcMap = {};
+
+  Object.values(master.styles).forEach(style => {
+
+    Object.values(style.skus).forEach(sku => {
+
+      const fc = sku.sku.includes("FBA")
+        ? "FBA"
+        : sku.sku.includes("FBF")
+        ? "FBF"
+        : sku.sku.includes("SJIT")
+        ? "SJIT"
+        : "SELLER";
+
+      if (!fcMap[fc]) {
+        fcMap[fc] = {
+          totalStock: 0,
+          totalUnits: 0
+        };
+      }
+
+      fcMap[fc].totalStock += sku.totalStock;
+      fcMap[fc].totalUnits += sku.totalSales;
+    });
+  });
+
+  let grandStock = 0;
+  let grandUnits = 0;
+
+  const rows = Object.keys(fcMap).map(fc => {
+
+    const totalStock = fcMap[fc].totalStock;
+    const totalUnits = fcMap[fc].totalUnits;
+
+    const drr = totalDays > 0 ? totalUnits / totalDays : 0;
+    const sc = drr > 0 ? totalStock / drr : 0;
+
+    grandStock += totalStock;
+    grandUnits += totalUnits;
+
+    return {
+      fc,
+      totalStock,
+      totalUnits,
+      drr,
+      sc
+    };
+  });
+
+  const grandDRR = totalDays > 0 ? grandUnits / totalDays : 0;
+  const grandSC = grandDRR > 0 ? grandStock / grandDRR : 0;
+
   computedStore.summaries.fcStock = {
-    totalStock,
-    totalProduction
+    rows,
+    grandStock,
+    grandUnits,
+    grandDRR,
+    grandSC
   };
 }
